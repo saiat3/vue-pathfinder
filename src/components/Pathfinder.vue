d32cc22bda36689126ae79cdfc60cfe4ab42d09a
Saiat Kalbiev <kalbievich11@gmail.com>
2019-10-07 12:38:05 +0400

    Implement Node and Pathfinder components

---
+++ src/components/Pathfinder.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="pathfinder">
+    <node v-for="index in 945"
+          :index="index"
+          :key="index"></node>
+  </div>
+</template>
+
+<script>
+  import Node from "./node/Node";
+
+  export default {
+    name: 'Pathfinder',
+    components: {Node},
+  }
+</script>
+
+<style>
+  #pathfinder {
+    font-size: 0;
+    border: 1px solid black;
+    width: 1890px;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/node/Node.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="node">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Node',
+    props: {
+      index: {
+        type: Number
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  #node {
+    width: 40px;
+    height: 40px;
+    background-color: #ffb6c3;
+    border: 1px solid black;
+    display: inline-block;
+    text-align: center;
+
+
+    &:hover {
+      background-color: red;
+      cursor: pointer;
+    }
+
+
+  }
+</style>
